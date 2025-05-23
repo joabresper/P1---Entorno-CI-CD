8ae36547ecce4b0cdc7680ac4661d9607fcb67bb
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-05-23 19:36:16 -0300

    deploy in render configured

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import app from './app.js';
 
 if (process.env.NODE_ENV !== 'test') {
+  
+  const port = process.env.PORT || 3000;
+
   app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+    console.log('Servidor corriendo en http://localhost:${port}');
   });
 }
 
