98da63ff8aceb48844cdcef9cf2e404d2536f77e
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-05-24 17:05:13 -0300

    new charge

--- src/app.js
+++ src/app.js
@@ -8,4 +8,13 @@ app.get('/', (req, res) => {
   res.json({ mensaje });
 });
 
+app.get('/download', (req, res) => {
+  const bytes = 10 * 1024 * 1024; // 10 MB
+  const buffer = Buffer.alloc(bytes, 'a');
+
+  res.setHeader('Content-Disposition', 'attachment; filename="dummy.txt"');
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(buffer);
+});
+
 export default app;
--- src/saludo.js
+++ src/saludo.js
@@ -1,34 +1,34 @@
-import { calcularPrimos, consumirMemoria,  } from './carga-pesada.js';
-
-// function saludar() {
-// 	const hora = new Date().getHours();
-  
-// 	if (hora >= 6 && hora < 12) {
-// 	  return '¡Buenos días!🌞';
-// 	} else if (hora >= 12 && hora < 14) {
-// 	  return '¡Buen provecho!🍴';
-// 	} else if (hora >= 14 && hora < 20) {
-// 	  return '¡Buenas tardes!🍵';
-// 	} else {
-// 	  return '¡Buenas noches!🌛';
-// 	}
-// }
+// import { calcularPrimos, consumirMemoria,  } from './carga-pesada.js';
 
 function saludar() {
 	const hora = new Date().getHours();
-
-	calcularPrimos(800);
-	consumirMemoria(200);
-
+  
 	if (hora >= 6 && hora < 12) {
-		return '¡Buenos días!🌞';
+	  return '¡Buenos días!🌞';
 	} else if (hora >= 12 && hora < 14) {
-		return '¡Buen provecho!🍴';
+	  return '¡Buen provecho!🍴';
 	} else if (hora >= 14 && hora < 20) {
-		return '¡Buenas tardes!🍵';
+	  return '¡Buenas tardes!🍵';
 	} else {
-		return '¡Buenas noches!🌛';
+	  return '¡Buenas noches!🌛';
 	}
 }
+
+// function saludar() {
+// 	const hora = new Date().getHours();
+
+// 	calcularPrimos(800);
+// 	consumirMemoria(200);
+
+// 	if (hora >= 6 && hora < 12) {
+// 		return '¡Buenos días!🌞';
+// 	} else if (hora >= 12 && hora < 14) {
+// 		return '¡Buen provecho!🍴';
+// 	} else if (hora >= 14 && hora < 20) {
+// 		return '¡Buenas tardes!🍵';
+// 	} else {
+// 		return '¡Buenas noches!🌛';
+// 	}
+// }
 
 export default saludar;
