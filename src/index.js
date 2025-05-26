9cdd0a7e24cc6c0df15312a71cc14fe349d8f9f2
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-05-25 23:47:23 -0300

    preparacion para vercel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import app from './app.js';
 
-if (process.env.NODE_ENV !== 'test') {
-  
+if (process.env.NODE_ENV !== 'test' && process.env.VERCEL !== '1') {
   const port = process.env.PORT || 3000;
 
-  app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:${port}');
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
   });
 }
+
 
