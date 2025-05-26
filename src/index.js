ec93fbb65c3314960ad799c3302d651ded6c34c0
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-05-26 00:01:43 -0300

    a

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import app from './app.js';
 
-if (process.env.NODE_ENV !== 'test' && process.env.VERCEL !== '1') {
-  const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-  app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
 
 
