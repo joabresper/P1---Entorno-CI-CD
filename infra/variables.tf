4e3e40d6e5e12073afef74889ce74ad3d55b6424
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-06-01 20:30:28 -0300

    primera prueba terraform

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "Region donde se desplegara el recurso"
+  default     = "us-east-2"
+}
+
+variable "ami_id" {
+	description = "ID de la AMI"
+	default = "ami-06c8f2ec674c67112"
+}
+
+variable "instance_type" {
+	description = "Tipo de la instancia"
+	default = "t2.micro"
+}
