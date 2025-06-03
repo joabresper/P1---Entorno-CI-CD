c4d536d8e5e7109474376225f22be5606e0eb1d5
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-06-02 21:43:49 -0300

    a

--- infra/main.tf
+++ infra/main.tf
@@ -1,16 +1,13 @@
 terraform {
-  cloud {
-    organization = "Entorno-CI-CD"
-    workspaces {
-      name = "infra-develop"
+  cloud {}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
     }
   }
-
   required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region  = var.region
 }
 
 resource "aws_instance" "app_server" {
