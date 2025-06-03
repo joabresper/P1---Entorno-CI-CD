dbcc30d06326b451d034d4d6a687c532bfda69a6
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-06-03 16:34:57 -0300

    prueba deploy

--- infra/main.tf
+++ infra/main.tf
@@ -29,3 +29,8 @@ resource "aws_instance" "app_server" {
     Name = "EC2-instance-for-tests"
   }
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+  description = "IP pública de la instancia EC2"
+}
