8d1a79e59c06844341ebbda7785150b3a7d429a0
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-06-09 22:46:58 -0300

    a

--- infra/main.tf
+++ infra/main.tf
@@ -30,6 +30,26 @@ resource "aws_instance" "dev_test_server" {
   }
 }
 
+resource "aws_instance" "infra_test" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  count         = var.environment == "infra" ? 1 : 0
+
+  user_data = <<-EOF
+              #!/bin/bash
+              mkdir -p /home/ubuntu/.ssh
+              echo "${var.ssh-public-key}" >> /home/ubuntu/.ssh/authorized_keys
+              chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+              chmod 600 /home/ubuntu/.ssh/authorized_keys
+              EOF
+  
+  vpc_security_group_ids = ["sg-0664764c4b4d81bac"]
+
+  tags = {
+    Name = "EC2-infra-test"
+  }
+}
+
 resource "aws_instance" "production_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
@@ -52,5 +72,7 @@ resource "aws_instance" "production_server" {
 
 output "ec2_public_ip" {
   description = "IP pública de la instancia EC2"
-  value = var.environment == "production" ? aws_instance.production_server[0].public_ip : aws_instance.dev_test_server[0].public_ip
+  value = var.environment == "production" ? aws_instance.production_server[0].public_ip :
+          var.environment == "develop"    ? aws_instance.dev_test_server[0].public_ip :
+                                            aws_instance.infra_test_server[0].public_ip
 }
