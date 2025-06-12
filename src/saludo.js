6c8bf79de93036dce240b022fe5b5c3bd27b2092
Joaquin Conrado Bregant <joabresper@gmail.com>
2025-06-11 22:19:57 -0300

    a

--- src/saludo.js
+++ src/saludo.js
@@ -48,4 +48,37 @@ function saludar3() {
 	}
 }
 
+function saludar4() {
+	const hora = new Date().getHours();
+
+	calcularPrimos(800);
+	consumirMemoria(200);
+
+	if (hora >= 6 && hora < 12) {
+		return '¡Buenos días!🌞';
+	} else if (hora >= 12 && hora < 14) {
+		return '¡Buen provecho!🍴';
+	} else if (hora >= 14 && hora < 20) {
+		return '¡Buenas tardes!🍵';
+	} else {
+		return '¡Buenas noches!🌛';
+	}
+}
+
+function saludar4() {
+	const hora = new Date().getHours();
+
+	calcularPrimos(800);
+	consumirMemoria(200);
+
+	if (hora >= 6 && hora < 12) {
+		return '¡Buenos días!🌞';
+	} else if (hora >= 12 && hora < 14) {
+		return '¡Buen provecho!🍴';
+	} else if (hora >= 14 && hora < 20) {
+		return '¡Buenas tardes!🍵';
+	} else {
+		return '¡Buenas noches!🌛';
+	}
+}
 export default saludar;
